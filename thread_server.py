ab8d42690489041116a535d03d15e77f742e7c4e
5730175121-NR <nuttapol7185@gmail.com>
2017-11-14 20:21:39 +0700

    add new header

--- thread_server.py
+++ thread_server.py
@@ -11,7 +11,6 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         print(threading.currentThread().getName() + " is working")
         start = time.time()
-        message = ''
         dict_data = {}
         parsed_path = parse.urlparse(self.path)
         path= self.path
@@ -19,15 +18,13 @@ class Handler(BaseHTTPRequestHandler):
         querys = parsed_path.query 
         if(querys != '' and realpath != '/favicon.ico'):
             dict_data['data'] = navigator(realpath,querys)
-            message = json.dumps(dict_data)
         end = time.time()
         print('request respone in : %s secs' % str('%.3f' % (end - start)))
         self.send_response(200)
-        self.send_header('Content-Type',
-                         'text/plain; charset=utf-8')
+        self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-        self.wfile.write(message.encode('utf-8'))       
+        self.wfile.write(json.dumps(dict_data).encode('utf-8'))       
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
@@ -60,5 +57,6 @@ if __name__ == '__main__':
         server.serve_forever()
     except KeyboardInterrupt:
         pass
+    cleanup_stop_thread()
     server.server_close()
     print('server is closed.')
